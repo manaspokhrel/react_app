81c99e1f185d11e9d4ce65bd7e0455a50a53a979
Manoj Pokhrel <manojpokhrel2057@gmail.com>
2022-08-21 01:43:52 +0545

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Button from './Button'
+const App=()=>{
+    return <div>
+       <h1>Web Hub</h1>
+       <h3>Which website would you like to open?</h3> 
+      <Button name = "Google" link = "hpps://www.google.com"/> 
+      <Button name = "Facebook" link = "hpps://www.fb.com"/>
+      <Button name = "MeroShare" link = "hpps://www.meroshare.cdsc.com.np"/>
+      <Button name = "Youtube" link = "hpps://www.youtube.com"/>
+    </div> 
+}
+export default App;
